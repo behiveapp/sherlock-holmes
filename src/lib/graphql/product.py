16d1292564602da1b2b899ea43fd641319bed63b
Edson Júnior <ebf.junin@gmail.com>
2018-04-09 00:35:57 -0300

    Added query support to categories field

--- src/lib/graphql/product.py
+++ src/lib/graphql/product.py
@@ -3,9 +3,10 @@ from sanic import Sanic
 
 def get_product(hit):
   data = hit['_source']
-  return Product(sellerIdentifier=data['seller_identifier'], code=data['code'], name=data['name'])
+  return Product(sellerIdentifier=data['seller_identifier'], code=data['code'], name=data['name'], categories=data['categories'])
 
 class Product(graphene.ObjectType):
   sellerIdentifier = graphene.String()
   name = graphene.String()
   code = graphene.String()
+  categories = graphene.List(graphene.String)
--- src/lib/graphql/schema.py
+++ src/lib/graphql/schema.py
@@ -6,12 +6,13 @@ from lib.graphql.product import Product, get_product
 
 class Query(graphene.ObjectType):
   Sellers = graphene.Field(graphene.List(lambda: Seller), name=graphene.String())
-  Products = graphene.Field(graphene.List(lambda: Product), name=graphene.String())
+  Products = graphene.Field(graphene.List(lambda: Product), name=graphene.String(), category=graphene.String())
 
   def resolve_Sellers(self, info, name):
     return list(map(get_seller, SellerModel.get_results({'short_name': name})))
   
-  def resolve_Products(self, info, name):
-    return map(get_product, ProductModel.get_results({'name': name}))
+  def resolve_Products(self, info, name = None, category = None):
+    query = {'name': name} if name else {'categories': category}
+    return map(get_product, ProductModel.get_results(query))
 
 schema = graphene.Schema(query=Query)
